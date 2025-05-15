42e04170246998cfbf9db228905cea48db6894e8
Marcelo Neiva <marceloneiva@yahoo.com.br>
2025-05-15 19:08:21 -0300

    enviando os arquivos qu estão faltando

---
+++ internal/api/handlers/user_handler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Lista de usuários",
+	})
+}
